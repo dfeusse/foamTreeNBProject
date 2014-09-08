dc228363592ec42887e054f7d60ef04e444b4177
Dan Feusse <dfeusse@gmail.com>
2014-09-08 00:48:01 -0700

    all js and angular files working

--- js/service.js
+++ js/service.js
@@ -1,9 +1,11 @@
+
 main.factory('CallApiService', function($resource){
 	//return $resource('https://www.googleapis.com/books/v1/volumes?q=:action&key=AIzaSyB6xs-eZW2RbaNjsZyUi7Q0QLQWj3yoLiU');
 	//return $resource('https: ... q=:action');
 	//return $resource('https://admin.netbase.com/ariel/ariel/cb/TestDir/michael-wishesv1.json?pullsize=1000&domain=babycenter.com');
 	return $resource('https://www.googleapis.com/books/v1/volumes?q=rand&key=AIzaSyB6xs-eZW2RbaNjsZyUi7Q0QLQWj3yoLiU');
 });
+
 
 /*
 main.controller('CallApiController', function($scope, CallApiService) {
--- js/uploadFileController.js
+++ js/uploadFileController.js
@@ -1,6 +1,15 @@
 //var UploadFileController = ['$scope', function($scope) {
+/*
+main.factory('CallApiService', function($resource){
+    //return $resource('https://www.googleapis.com/books/v1/volumes?q=:action&key=AIzaSyB6xs-eZW2RbaNjsZyUi7Q0QLQWj3yoLiU');
+    //return $resource('https: ... q=:action');
+    //return $resource('https://admin.netbase.com/ariel/ariel/cb/TestDir/michael-wishesv1.json?pullsize=1000&domain=babycenter.com');
+    return $resource('https://www.googleapis.com/books/v1/volumes?q=rand&key=AIzaSyB6xs-eZW2RbaNjsZyUi7Q0QLQWj3yoLiU');
+});
+*/
+
+main.controller('UploadFileController', function($scope, CallApiService){
 //main.controller('UploadFileController', function($scope){
-main.controller('UploadFileController', function($scope){
 
 	$scope.uploadFile = function(){
 		var file = $scope.myFile;
@@ -88,5 +97,16 @@ main.controller('UploadFileController', function($scope){
     //$scope.data = [{"x":1,"y":166},{"x":2,"y":386},{"x":3,"y":235},{"x":4,"y":36},{"x":5,"y":46}];
     $scope.data = "uploaded data shown here"
 	document.getElementById('dataResult').innerHTML = JSON.stringify($scope.data);
+
+    $scope.callApi = function () {
+        //var type = $scope.mediaType;
+        //if ($scope.mediaType=="all")  type="";
+        //MediaService.get({term:$scope.searchTerm,entity:type},function(response){
+        CallApiService.get({action:$scope.searchTerm},function(response){
+            //$scope.pics = response.items;
+            console.log('$$$$$$$$$$$$$$$')
+            console.log(response)
+        });
+    }
 
 });
